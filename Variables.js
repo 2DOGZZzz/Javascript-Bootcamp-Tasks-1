49a921e40bd047153efbc7131c63a162ecd4455e
2DOGZZzz <74018834+2DOGZZzz@users.noreply.github.com>
2026-05-27 06:37:56 +0200

    Create Variables.js

---
+++ Variables.js
@@ -0,0 +1,22 @@
+//  1
+let name= 'Thapelo' ;
+let age= 35;
+let isStudent= true;
+console.log(name,age,isStudent);
+
+//  2
+let favouriteColour= 'blue';
+console.log(favouriteColour);
+favouriteColour='Red';
+console.log(favouriteColour);
+
+// 3
+let num1= 5;
+let num2= 4;
+let sum= num1+num2;
+let difference= num1-num2;
+let product= num1*num2;
+let quotient= num1/num2;
+
+console.log(sum,difference,product, quotient);
+
