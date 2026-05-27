c69cbb974a4dc22dd463538a558016084d03dcbf
2DOGZZzz <74018834+2DOGZZzz@users.noreply.github.com>
2026-05-27 06:40:59 +0200

    Create Datatypes_operators.js

---
+++ Datatypes_operators.js
@@ -0,0 +1,36 @@
+//Data type and operators 
+// Assessment 1
+let value1= 10;
+let value2= 4.56;
+let addition= value1 + value2;
+let subtraction= value1 - value2;
+let multiplication= value1 * value2;
+let division= value1/ value2;
+let modulus= value1%value2;
+let exponential= value1**value2;
+
+console.log(addition,subtraction, multiplication,division,modulus,exponential)
+
+// Assessment 2
+
+let x = 8;
+let y = 12;
+console.log(x>y);
+console.log(x<=y);
+console.log(x===y);
+console.log(x!==y);
+
+// 3
+let a = true;
+let b = false;
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
+
+// 4
+let p = 10;
+console.log(p += 5);
+console.log(p -= 5);
+console.log(p *= 5);
+console.log(p /= 5);
+console.log(p %= 5);
